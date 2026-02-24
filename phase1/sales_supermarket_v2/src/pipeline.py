9198e4fb7339b3226418c1fde3981b3c3715763c
Martin Papaleo <mpapaleo@itba.edu.ar>
2026-02-24 19:17:18 -0300

    changes

--- phase1/sales_supermarket_v2/src/pipeline.py
+++ phase1/sales_supermarket_v2/src/pipeline.py
@@ -1,10 +1,10 @@
 from ingest import load_raw_data
 from clean import clean_raw_data
 from validate import valid_clean_data
+from validate import value_pos_in_df
 import pandas as pd
 df_raw_ = load_raw_data()
 df_clean = clean_raw_data(df_raw_)
-
 
 if valid_clean_data(df_clean):
     print('Yes')
--- phase1/sales_supermarket_v2/src/validate.py
+++ phase1/sales_supermarket_v2/src/validate.py
@@ -1,2 +1,56 @@
-from clean.py import df_clean
-print(df_clean.head(5))
+'''
+0 <= Rating <= 10
+
+Datetime is datetime dtype
+
+'''
+from ingest import load_raw_data
+from clean import clean_raw_data
+import pandas as pd
+df_raw_ = load_raw_data()
+df_clean = clean_raw_data(df_raw_)
+
+# DELETE ALL CODE ABOVE
+def value_pos_in_df(df_, col_name):
+    if df_.loc[df_[col_name] <= 0].count().sum() != 0:
+        return False
+    return True
+
+def valid_clean_data(data_):
+    # No duplicate Invoice_ID
+    if data_['Invoice_ID'].shape != data_['Invoice_ID'].value_counts().shape:
+        print('False1')
+        return False
+
+    # No nulls in required columns (your full list)
+    if data_.isnull().sum().sum() != 0:
+        print('False2')
+        return False
+    
+    # Unit_price > 0 
+    if not value_pos_in_df(data_, 'Unit_price'):
+        print('False3')
+        return False
+
+    # Total > 0
+    if not value_pos_in_df(data_, 'Total'):
+        print('False4')
+        return False
+
+    # cogs > 0
+    if not value_pos_in_df(data_, 'cogs'):
+        print('False5')
+        return False
+    # I do know that Unit_price, Total and cogs could be chacked in one single line, but I'd want to differenciate with a differet print each possible fail
+
+    # Quantity >= 1
+    if data_.loc[data_['Quantity'] < 1].count().sum() != 0:
+        print('False6')
+        return False
+    
+    # 0 <= Rating <= 10
+    if data_.loc[(data_['Rating'] > 10) | (data_['Rating'] < 0)].count().sum() != 0:
+        print('False7')
+        return False
+    return True
+valid_clean_data(df_clean)
