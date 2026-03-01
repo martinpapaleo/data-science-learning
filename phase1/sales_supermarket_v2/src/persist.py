d9e5a7a517c44738937234cd906fcc18ea820735
Martin Papaleo <mpapaleo@itba.edu.ar>
2026-03-01 15:14:15 -0300

    changes

--- phase1/sales_supermarket_v2/src/persist.py
+++ phase1/sales_supermarket_v2/src/persist.py
@@ -1,3 +1,15 @@
+from pathlib import Path
+# Get current file directory (data/src/)
+BASE_DIR = Path(__file__).resolve().parent
+
+# Move up one level to data/
+DATA_DIR = BASE_DIR.parent
+
+# Define processed folder
+OUTPUT_DIR = DATA_DIR / "data" / "processed"
+
+# Save file
+
 def save_data(data_, debug=False):
-    data_.to_parquet()
+    data_.to_parquet(OUTPUT_DIR / "sales_clean.parquet", index=False)
     return True
