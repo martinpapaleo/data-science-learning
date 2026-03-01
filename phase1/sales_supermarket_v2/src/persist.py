51337c208da0b7d678464ae96c9375dfb5b5d91c
Martin Papaleo <mpapaleo@itba.edu.ar>
2026-03-01 14:53:17 -0300

    changes

--- phase1/sales_supermarket_v2/src/persist.py
+++ phase1/sales_supermarket_v2/src/persist.py
@@ -1,4 +1,3 @@
 def save_data(data_, debug=False):
-    
-
+    data_.to_parquet()
     return True
--- phase1/sales_supermarket_v2/src/pipeline.py
+++ phase1/sales_supermarket_v2/src/pipeline.py
@@ -2,12 +2,18 @@ from ingest import load_raw_data
 from clean import clean_raw_data
 from transform import transform_data
 from validate import valid_data
+from persist import save_data
 import pandas as pd
 
-df_raw_ = load_raw_data(debug=False)
-df_clean = clean_raw_data(df_raw_, debug=False)
-df_trans = transform_data(df_clean)
-valid_data(df_trans)
+def main():
 
-print('DF Valid')
-print(df_trans.head(5))
+    df_raw_ = load_raw_data(debug=False)
+    df_clean = clean_raw_data(df_raw_, debug=False)
+    df_trans = transform_data(df_clean)
+    valid_data(df_trans)
+    save_data(df_trans, debug=False)
+    print('Pipeline finished succesfully.')
+    print(df_trans.head(5))
+
+if __name__ == "__main__":
+    main()
