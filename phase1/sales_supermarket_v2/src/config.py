e515d2b720f0cd7219933174da63c7caa680eb2e
martinpapaleo <martinpapaleo1@gmail.com>
2026-03-02 23:25:22 -0300

    changes

--- phase1/sales_supermarket_v2/src/config.py
+++ phase1/sales_supermarket_v2/src/config.py
@@ -2,3 +2,10 @@ from pathlib import Path
 import pandas as pd
 import os
 
+ROOT_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = ROOT_DIR / 'data'
+RAW_DIR = DATA_DIR / 'raw'
+PROCESSED_DIR = DATA_DIR / 'processed'
+RAW_FILENAME = RAW_DIR / 'supermarket_sales.csv'
+OUTPUT_FILENAME = PROCESSED_DIR / 'sales_clean.parquet'
+debug = False
